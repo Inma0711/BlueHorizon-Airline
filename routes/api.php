0f8992ceee3f676f016ecc6e0db6a9514428fcf3
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-03 22:07:10 +0100

    feat: add web and api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*
+Route::get('/members', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/members', [MemberController::class, 'show'])->name('apimembersshow');
+    Route::get('/flights', [FlightController::class, 'index'])->name('apiflightshome');
+    Route::post('/reservations', [ReservationController::class, 'store'])->name('apireservationsstore');
+    Route::delete('/reservations/{reservation_id}', [ReservationController::class, 'destroy'])->name('apireservationsdestroy');
+});
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/flights', [FlightsController::class, 'index'])->name('flightshome');
+Route::get('/reservations/{flight_id}', [ReservationController::class, 'create'])->name('reservationscreate');
+Route::post('/reservations/{flight_id}', [ReservationController::class, 'store'])->name('reservationsstore');
