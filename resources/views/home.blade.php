c9904504cba0a57ff246d56f1950381f5101d1ce
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-04 17:07:15 +0100

    feat: create home view and layout folder

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Bienvenido a BlueHorizon Airline ✈</h1>
+    <p>Explora nuestros vuelos y reserva tu asiento.</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('title', 'BlueHorizon')</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    
+    <body>
+        <div id="app">
+           
+
+            <main>
+            @yield('content')
+            </main>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+    </body>
+</html>
