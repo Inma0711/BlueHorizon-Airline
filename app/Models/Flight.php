c3dd731f9db726fcaef6b08239607497c29eb713
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-04 16:51:06 +0100

    feat: add home and flight controller

---
+++ app/Models/Flight.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Flight extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'plane_id', 
+        'date', 
+        'departure_location', 
+        'arrival_location', 
+        'available_seats',
+        'status'
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FlightController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/flights', [FlightsController::class, 'index'])->name('flightsindex');
+Route::get('/flights', [FlightController::class, 'index'])->name('flights.index');
