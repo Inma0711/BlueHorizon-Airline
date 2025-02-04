7fd42ce5569512f4eee2b0a2f813086e1d075eb6
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-04 23:20:42 +0100

    style: add style body home

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,16 +1,13 @@
 <header>
     <nav class="navbar">
         <ul class="navbar-container">
-            <!-- Logo y Enlace a la izquierda -->
             <li class="navbar-item left">
                 <a href="{{ route('home') }}">
                     <img src="{{ asset('img/icons/logoWeb.png') }}" alt="Web logo" />
                 </a>
-                <!-- Enlace "Vuelos" al lado del logo -->
                 <a href="" class="menu-link">Vuelos</a>
             </li>
-            
-            <!-- Icono de Usuario a la derecha -->
+
             <li class="navbar-item right">
                 <a href="#">
                     <img src="{{ asset('img/icons/user.png') }}" alt="BlueHorizon icon" />
